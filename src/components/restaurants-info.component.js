eceb4479cfbd58d968eba18ee9ea683c8cc28771
devynqualls <devyn.qualls@gmail.com>
2023-06-24 23:38:52 -0400

    First Commit create standards

--- src/components/restaurants-info.component.js
+++ src/components/restaurants-info.component.js
@@ -1,22 +1,52 @@
 import React from "react";
-import { Text, StyleSheet } from "react-native";
+import styled from "styled-components/native";
+import { Text, StyleSheet, Image, View } from "react-native";
 import { Card } from "react-native-paper";
-import { styled } from "styled-components/native";
+import { SvgXml } from "react-native-svg";
+import star from "../../assets/star";
+import open from "../../assets/open.js"
+import { Spacer } from "./spacer/spacer.component";
 
 
-const title = styled.Text`
-padding: 16px;
-color: red;
+const RestaurantCard = styled(Card)`
+  background-color: ${(props) => props.theme.colors.bg.primary};
 `;
 
-const restaurantCard = styled(Card)`
-backgroundColor: "white"
-`
+const RestaurantCardCover = styled(Card.Cover)`
+  padding: ${(props) => props.theme.space[3]};
+  background-color: ${(props) => props.theme.colors.bg.primary};
+`;
 
-const restaurantCover = styled(Card.Cover)`
-backgroundColor: "white";
-padding: 20px;
-`
+const Address = styled(Text)`
+  font-family: ${(props) => props.theme.fonts.body};
+  font-size: ${(props) => props.theme.fontSizes.caption};
+`;
+
+const Title = styled(Text)`
+  font-family: ${(props) => props.theme.fonts.heading};
+  font-size: ${(props) => props.theme.fontSizes.body};
+  color: ${(props) => props.theme.colors.ui.primary};
+`;
+
+const Info = styled.View`
+  padding: ${(props) => props.theme.space[3]};
+`;
+
+const Rating = styled.View`
+  flex-direction: row;
+  padding-top: ${(props) => props.theme.space[2]};
+  padding-bottom: ${(props) => props.theme.space[2]};
+`;
+
+const Section = styled.View`
+  flex-direction: row;
+  align-items: center;
+`;
+const SectionEnd = styled.View`
+  flex: 1;
+  flex-direction: row;
+  justify-content: flex-end;
+`;
 
 export const RestaurantInfoCard = ({ restaurant = {} }) => {
   const {
@@ -31,16 +61,35 @@ export const RestaurantInfoCard = ({ restaurant = {} }) => {
     isClosedTemporarily,
   } = restaurant;
 
+  const ratingArray = Array.from(new Array(Math.floor(rating)));
+
   return (
-    <Card elevation={5} style={restaurantCard}>
-      <Card.Cover key={name} style={restaurantCover} source={{ uri: photos[0] }} />
-      <Text style={title}>{name}</Text>
-    </Card>
+    <RestaurantCard elevation={5}>
+      <RestaurantCardCover key={name} source={{ uri: photos[0] }} />
+      <Info>
+        <Title>{name}</Title>
+        <Section>
+          <Rating>
+            {ratingArray.map(() => (
+              <SvgXml xml={star} width={20} height={20} />
+            ))}
+          </Rating>
+          <SectionEnd>
+            {isClosedTemporarily && (
+              <Text variant="label" style={{ color: "red" }}>
+                CLOSED TEMPORARILY
+              </Text>
+            )}
+              <Spacer position="left" size="large">
+              {isOpenNow && <SvgXml xml={open} width={20} height={20} />}
+            </Spacer>
+            <Spacer position="left" size="large">
+              <Image style={{ width: 15, height: 15 }} source={{ uri: icon }} />
+            </Spacer>
+          </SectionEnd>
+        </Section>
+        <Address>{address}</Address>
+      </Info>
+    </RestaurantCard>
   );
 };
-
-// const styles = StyleSheet.create({
-//   card: {  },
-//   cover: { padding: 20, backgroundColor: "white" },
-//   title: { padding: 16 },
-// });
--- src/screens/restaurants-screen.js
+++ src/screens/restaurants-screen.js
@@ -2,31 +2,44 @@ import React from "react";
 import { Searchbar } from "react-native-paper";
 import { StatusBar, StyleSheet, SafeAreaView, Text, View } from "react-native";
 import { RestaurantInfoCard } from "../components/restaurants-info.component";
+import { styled } from "styled-components/native";
+
+const SafeArea = styled(SafeAreaView)`
+  flex: 1;
+  ${StatusBar.currentHeight && `margin-top: ${StatusBar.currentHeight}px`};
+`
+const SearchContainer = styled.View`
+  padding: ${(props) => props.theme.space[3]};
+`
+const RestaurantListContainer = styled.View`
+  flex: 1;
+  padding: ${(props) => props.theme.space[3]};
+  backgroundColor: ${(props) => props.theme.colors.bg.secondary}
+  `
 
 export const RestaurantsScreen = () => (
-  <SafeAreaView style={styles.container}>
-    <View style={styles.search}>
+  <SafeAreaView>
+    <View >
       <Searchbar />
     </View>
-    <View style={styles.list}>
+    <View >
       <RestaurantInfoCard />
     </View>
   </SafeAreaView>
 );
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    marginTop: StatusBar.currentHeight,
-  },
-  search: {
-    padding: 16,
-  },
-  list: {
-    flex: 1,
-    padding: 16,
-    backgroundColor: "blue",
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     marginTop: StatusBar.currentHeight,
+//   },
+//   search: {
+//     padding: 16,
+//   },
+//   list: {
+//     flex: 1,
+//     padding: 16,
+//     backgroundColor: "blue",
+//   },
+// });
 
-// next goal is to add passable props to the outer componenet
